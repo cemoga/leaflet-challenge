f3e0fe74463b6beaf4cd08ff5d4c5f2a386bcb08
Cesar <cemoga@yahoo.com>
2019-12-10 12:11:45 -0500

    Step 1 Concluded

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -12,17 +12,17 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
 }).addTo(myMap);
 
-
+// API
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
 
 console.log(URL)
 
-
+// Modify the size of the Markers
 function markerSize(mag) {
     return mag * 70000;
 }
 
+// Return max and minimun magnitudes and create buckets for colors
 d3.json(URL, function (response) {
     var magnitudes = []
     for (var i = 0; i < response.metadata.count; i++) {
@@ -53,21 +53,30 @@ d3.json(URL, function (response) {
     bucket_4 = minMag + incColor * 4
     bucket_5 = minMag + incColor * 5
     bucket_6 = minMag + incColor * 6
-
-    console.log("bucket_1:", bucket_1)
-
 });
 
-
+// Create the markers and assing them a color
 d3.json(URL, function (response) {
     console.log("Total Records: ", response.metadata.count)
     console.log("Latitude 1st: ", response.features[0].geometry.coordinates[1])
     console.log("Long 1st: ", response.features[0].geometry.coordinates[0])
     console.log("Magnitude 1st: ", response.features[0].properties.mag)
-    console.log("Magnitude 1st: ", response.features[0].properties.place)
+    console.log("Place: ", response.features[0].properties.place)
+
+    var reportUpdateMilisecs = response.metadata.generated
+    reportUpdate = new Date(reportUpdateMilisecs)
+    reportUpdateDate = reportUpdate.toLocaleDateString('en-US')
+    reportUpdateTime = reportUpdate.toLocaleTimeString('en-US')
+
+    console.log("Report Update: ", reportUpdateDate, reportUpdateTime)
+
 
     for (var i = 0; i < response.metadata.count; i++) {
         var mag = response.features[i].properties.mag
+        var miliseconds = response.features[i].properties.time
+        var date = new Date(miliseconds)
+        var dateSpot = date.toLocaleDateString('en-US')
+        var timeSpot = date.toLocaleTimeString('en-US')
         var place = response.features[i].properties.place
         var lat = response.features[i].geometry.coordinates[1]
         var long = response.features[i].geometry.coordinates[0]
@@ -75,22 +84,22 @@ d3.json(URL, function (response) {
 
         var color = "";
         if (mag < bucket_1) {
-            var color = "#2ca121";
+            var color = "#00CC10";
         }
         else if (mag < bucket_2) {
-            var color = "#00ff30";
+            var color = "#48D100";
         }
         else if (mag < bucket_3) {
-            var color = "#fff000";
+            var color = "#A6D600";
         }
         else if (mag < bucket_4) {
-            var color = "#ffcc00";
+            var color = "#DBAD00";
         }
         else if (mag < bucket_5) {
-            var color = "#ff7200";
+            var color = "#E05100";
         }
-        else if (mag < bucket_6+1) {
-            var color = "#ff0000";
+        else {
+            var color = "#E5000E";
         }
 
         L.circle(location, {
@@ -102,7 +111,35 @@ d3.json(URL, function (response) {
             // Setting our circle's radius equal to the output of our markerSize function
             // This will make our marker's size proportionate to its points
             radius: markerSize(mag)
-        }).bindPopup("<h1>" + place + "</h1> <hr> <h3>Magnitude: " + mag).addTo(myMap);
-
+        }).bindPopup("<h3>" + place + "</h3> <hr> <p><b>Magnitude: </b>" + mag +
+            "<br><b>Date: </b>" + dateSpot +
+            "<br><b>Time: </b>" + timeSpot).addTo(myMap);
+        // Call the updateLegend function, which will... update the legend!
+        updateLegend(reportUpdate);
     }
 });
+
+// Create a legend to display information about our map
+var info = L.control({
+    position: "bottomright"
+});
+
+// When the layer control is added, insert a div with the class of "legend"
+info.onAdd = function () {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
+// Add the info legend to the map
+info.addTo(myMap);
+
+function updateLegend(time) {
+    document.querySelector(".legend").innerHTML = [
+        "<p><b>Updated: </b> <br>" + reportUpdateDate +"<br>"+ reportUpdateTime + "</p><hr>",
+        "<p class='one'>" + "&#9632;" + "</p><p id='in-line'>" + "0-" + bucket_1 + "</p><br>",
+        "<p class='two'>" + "&#9632;" + "</p><p id='in-line'>" + bucket_1 + "-" + bucket_2 + "</p><br>",
+        "<p class='three'>" + "&#9632;" + "</p><p id='in-line'>" + bucket_2 + "-" + bucket_3 + "</p><br>",
+        "<p class='four'>" + "&#9632;" + "</p><p id='in-line'>" + bucket_3 + "-" + bucket_4 + "</p><br>",
+        "<p class='five'>" + "&#9632;" + "</p><p id='in-line'>" + bucket_4 + "-" + bucket_5 + "</p><br>",
+        "<p class='six'>" + "&#9632;" + "</p><p id='in-line'>" + "> " + bucket_5 + "</p>"
+    ].join("");
+}
